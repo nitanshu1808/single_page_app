c49c735edadc45eb9dd79482f36ea9e4ab25d808
Nitanshu Rehani <nitanshurehani@nitanshus-air.home>
2018-09-15 12:59:18 +0100

    - added application logo - used content_for for dynamic title

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,5 @@
+$(function() {
+  setTimeout(function(){
+    $('.flash-msg').slideUp(800);
+  }, 1500);
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # The method detects the level of class that needs to be displayed in the flash message
+  def flash_class(level)
+    case level
+      when "notice" then "alert alert-info"
+      when "success" then "alert alert-success"
+      when "error" then "alert alert-error"
+      when "alert" then "alert alert-error"
+    end
+  end
+
 end
